eb48bea7a4004bcd3e901710e4e05a71336d898e
3A532072 <dinel1138@gmail.com>
2018-11-09 01:42:03 +0800

    initial

---
+++ app/Http/Controllers/cool/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\cool;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function index()
+        return 'Cooler ';
+}
