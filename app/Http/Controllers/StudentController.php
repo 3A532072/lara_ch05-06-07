1b9938a0ddd6c6e8aaeab8ec93d9d23bd11a7381
3A532072 <dinel1138@gmail.com>
2018-11-14 07:20:04 +0800

    練習三

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,10 +6,14 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function getStudentData($student_no){
-        return "學號:".$student_no;
-    }
-    public function getStudentScore($student_no, $subject = null){
-        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+    return \View::make('score',[
+            'student_no'=>$student_no,
+            'subject'=>null
+        ]);
+    public function getStudentScore($student_no,$subject = null){
+        return \View::make('score',[
+            'student_no'=>$student_no,
+            'subject'=>$subject
+        ]);
     }
 }
